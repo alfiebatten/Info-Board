f6fb769b9df5577568547517368fde1c415d2d99
Alfiebatten <alfiebatten123@gmail.com>
2019-04-03 20:39:39 +0100

    Started validation for main page

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,3 +1,15 @@
 document.querySelector('[data-switch-contrast]').addEventListener('click', function() {
   document.body.classList.toggle('nightmode');
 });
+
+function checkforblank() {
+
+  if (document.getElementById('username').value == "") {
+    document.getElementById('username').style.borderColor = "red";
+    return false;
+  }
+  if (document.getElementById('password').value == "") {
+    document.getElementById('password').style.borderColor = "red";
+    return false;
+  }
+}
